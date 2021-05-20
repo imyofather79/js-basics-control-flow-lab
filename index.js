8ff43e13cfdfa53f87fde0e67d0b323629840c85
imyofather79 <chunsum708@gmail.com>
2021-05-20 17:35:19 -0600

    Completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,43 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(feet){
   // Write your code here!
+ let string;
+
+  if (feet <= 400){
+    string = `This one is on me!`
+  
+  } else if (feet < 2500){
+    string = `I will gladly take your thirty bucks.`
+  
+   } else if (feet >= 2500){
+    string = `No can do.`};
+
+return string;
+  }
+
+function ternaryCheckCity(city){
+  // Write your code here!
+let string;
+
+city === `NYC`? (string = `Ok, sounds good.`) : (string = `No go.`);
+
+return string;
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+
+let string;
+switch (tip) {
+  case `generous`:
+  string = `Thank you so much.`;
+  break;
+  case `not as generous`:
+  string = `Thank you.`;
+  break;
+  case `thanks for everything`:
+  string = `Bye.`;
+    break; 
+}
+return string;
 }
